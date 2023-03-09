6581c10f596f9a80573c2a2da511f24bb34e76fd
AndyNguyen002 <ngocdungokla@gmail.com>
2023-03-09 22:55:17 +0700

    thêm đăng nhập, đăng ký ( vẫn còn lỗi thông báo validate )

---
+++ PJWebNC/Dao/DaoNguoiDung.cs
@@ -0,0 +1,81 @@
+﻿using PJWebNC.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace PJWebNC.Dao
+{
+    public class DaoNguoiDung
+    {
+        public static NguoiDung getOne(string _taikhoan, string _matkhau)
+        {
+            NguoiDung objND = null;
+
+            string strConnection = ConfigurationManager.ConnectionStrings["ConnDB"].ConnectionString;
+
+            string sql = "select UserID, TaiKhoan, MatKhau, FullName, VaiTro from [NguoiDung] where TaiKhoan = @taikhoan and MatKhau = @matkhau";
+
+            using (SqlConnection conn = new SqlConnection(strConnection))
+            {
+                SqlCommand sqlCommand = new SqlCommand(sql, conn);
+                sqlCommand.Parameters.Add("@taikhoan", System.Data.SqlDbType.VarChar).Value = _taikhoan;
+                sqlCommand.Parameters.Add("@matkhau", System.Data.SqlDbType.VarChar).Value = _matkhau;
+                sqlCommand.CommandType = System.Data.CommandType.Text;
+
+                conn.Open();
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    objND = new NguoiDung();
+                    objND.UserID = Convert.ToInt32(reader["UserID"]);
+                    objND.TaiKhoan = Convert.ToString(reader["TaiKhoan"]);
+                    objND.MatKhau = Convert.ToString(reader["MatKhau"]);
+                    objND.FullName = Convert.ToString(reader["FullName"]);
+                    objND.VaiTro = Convert.ToInt32(reader["VaiTro"]);
+                }
+                reader.Close();//Đóng đối tượng DataReader
+                conn.Close();//Đóng kết nối
+                conn.Dispose();//Giải phóng bộ nhớ
+                return objND;
+            }
+        }
+        public static NguoiDung getExits(string _taikhoan)
+        {
+            NguoiDung objND = null;
+
+            string strConnection = ConfigurationManager.ConnectionStrings["ConnDB"].ConnectionString;
+
+            string sql = "select UserID, TaiKhoan, MatKhau, FullName, VaiTro from [NguoiDung] where TaiKhoan = @taikhoan ";
+
+            using (SqlConnection conn = new SqlConnection(strConnection))
+            {
+                SqlCommand sqlCommand = new SqlCommand(sql, conn);
+                sqlCommand.Parameters.Add("@taikhoan", System.Data.SqlDbType.VarChar).Value = _taikhoan;
+                
+                sqlCommand.CommandType = System.Data.CommandType.Text;
+
+                conn.Open();
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    objND = new NguoiDung();
+                    objND.UserID = Convert.ToInt32(reader["UserID"]);
+                    objND.TaiKhoan = Convert.ToString(reader["TaiKhoan"]);
+                    objND.MatKhau = Convert.ToString(reader["MatKhau"]);
+                    objND.FullName = Convert.ToString(reader["FullName"]);
+                    objND.VaiTro = Convert.ToInt32(reader["VaiTro"]);
+                }
+                reader.Close();//Đóng đối tượng DataReader
+                conn.Close();//Đóng kết nối
+                conn.Dispose();//Giải phóng bộ nhớ
+                return objND;
+            }
+        }
+    }
+}
--- PJWebNC/default.aspx
+++ PJWebNC/default.aspx
@@ -39,7 +39,7 @@
                 <p>Sản phẩm nổi bật</p>
                 <div class="typeProduct">
                     <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li class="nav-item" role="presentation">
+                        <li class="nav-item" style="background-color:white" role="presentation">
                             <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
                                 <h3>Nước hoa nam</h3>
                             </button>
@@ -60,10 +60,11 @@
                 <div class="tab-content mt-5" id="pills-tabContent">
                         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
                             <div class="Product">
-
+                                
                                 <asp:DataList ID="SanPham" runat="server" RepeatDirection="Horizontal">
                                     <ItemTemplate>
                                         <a href="ChiTietSanPham.aspx?id=<%#Eval("IDSanPham") %>">
+                                            <div class="swiper-slide">
                                             <div class="itemProduct">
                                                 <div class="picProduct">
                                                     <asp:Image ID="img1" runat="server" Width="200" Height="250" ImageUrl='<%# Eval("Anh") %>' />
@@ -74,9 +75,11 @@
 
                                                 <h4 style="text-align: center"><%#Eval("GiaBan") %> đ</h4>
                                             </div>
+                                                </div>
                                         </a>
                                     </ItemTemplate>
                                 </asp:DataList>
+                                    
                             </div>
                         </div>
                         <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
@@ -172,7 +175,7 @@
             <h3>Giờ mở cửa: Các ngày trong tuần từ 9:00 – 21:00</h3>
             <!-- info -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+            
         </div>
 
         <!-- content -->
